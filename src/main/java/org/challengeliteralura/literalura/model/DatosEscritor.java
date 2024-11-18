403f01a1341db0575007e2a49b0b3abb982e2cb0
Franco Robles <franco_robles94@outlook.com>
2024-11-18 15:26:51 -0300

    cambie las death y birth de string a Integer

--- src/main/java/org/challengeliteralura/literalura/model/DatosEscritor.java
+++ src/main/java/org/challengeliteralura/literalura/model/DatosEscritor.java
@@ -3,13 +3,10 @@ package org.challengeliteralura.literalura.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
-import java.util.Date;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosEscritor(
         @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year") String birth_year,
-        @JsonAlias("death_year") String death_year ){
+        @JsonAlias("birth_year") Integer birth_year,
+        @JsonAlias("death_year") Integer death_year ){
 
 }
