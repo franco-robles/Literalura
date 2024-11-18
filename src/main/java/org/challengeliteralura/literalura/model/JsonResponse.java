e1d55b7deb82418e6ee5c50a5b285b3abd3dbe67
Franco Robles <franco_robles94@outlook.com>
2024-11-18 15:30:25 -0300

    nada relevante

--- src/main/java/org/challengeliteralura/literalura/model/JsonResponse.java
+++ src/main/java/org/challengeliteralura/literalura/model/JsonResponse.java
@@ -3,7 +3,6 @@ package org.challengeliteralura.literalura.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
