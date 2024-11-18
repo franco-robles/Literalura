12e31b2435d8d4b1e3962e1a1fa3b2e9b515f826
Franco Robles <franco_robles94@outlook.com>
2024-11-18 15:34:55 -0300

    agregue existByTitulo y fndByIdiomas

--- src/main/java/org/challengeliteralura/literalura/repository/LibroRepository.java
+++ src/main/java/org/challengeliteralura/literalura/repository/LibroRepository.java
@@ -1,4 +1,15 @@
 package org.challengeliteralura.literalura.repository;
 
-public class LibroRepository {
+import org.challengeliteralura.literalura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+
+
+    boolean existsByTitulo(String titulo);
+
+
+    List<Libro> findByIdiomas(String idioma);
 }
